932028513c2858fd77612cc68460ed33b442ced6
HenryHell0 <71513462+HenryHell0@users.noreply.github.com>
2020-11-03 17:36:06 -0500

    Update script.js

--- matter/alotOfBoxes/script.js
+++ matter/alotOfBoxes/script.js
@@ -41,7 +41,7 @@ var mouseConstraint = MouseConstraint.create(engine, {
     }
 });
 
-engine.world.gravity.y = 0.5;
+engine.world.gravity.y = 0.25;
 World.add(engine.world, [ stack, mouseConstraint ] );
 Engine.run(engine);
 Render.run(render);
